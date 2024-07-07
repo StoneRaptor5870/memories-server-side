f61a9d45feb15b7985c2718ca11d4b5114300193
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-07-07 23:00:30 +0530

    cors

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ import userRoutes from "./routes/users.js";
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
 app.use(cors({
   origin: "http://localhost:3000",
   methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
