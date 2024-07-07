cdf894d6f2fc12b5abedf05279feeae0a3d8ab66
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-07-07 22:49:59 +0530

    cors

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.options('*', cors());
 
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
